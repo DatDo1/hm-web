ac5d412ea3c834760ceff5d383f6e83c3392afc6
DatDo1 <doducdat21122001@gmail.com>
2022-12-08 10:46:16 +0700

    update house-controller

--- app/Http/Controllers/adminC/HousesController.php
+++ app/Http/Controllers/adminC/HousesController.php
@@ -8,5 +8,13 @@ use App\Models\House;
 
 class HousesController extends Controller
 {
-    
+    private $house;
+    public function __construct(){
+        $this->house = new House();
+    }
+    public function index(){
+        $title = 'Danh sách nhà'; 
+        $housesList = House::all();// $this->house->getAllHouses();
+        return View('admin.houses', compact('title', 'housesList'));
+    }
 }
